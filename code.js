512af77163188787d875202fd1c1396f7148bf23
pfrithiof <pfrithiof7@gmail.com>
2022-05-27 07:38:47 +0200

    Försökt ändra bakgrundsfärg

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
+const changeColor = document.querySelector("#color");
+const inputText = document.querySelector("input");
+
+changeColor.addEventListener("change", function(event) {
+    console.log(event.target, event, event.target.value);
+
+    // Ãndra sidans style background-color backgroundColor
+    document.body.main.style.backgroundColor = event.target.value;
+
 function initMap() {
     
     const peepee = { lat: -47.1915, lng: 52.8371 };
@@ -6,7 +15,6 @@ function initMap() {
       zoom: 4,
       center: peepee,
     });
-    
     const marker = new google.maps.Marker({
       position: peepee,
       map: map,
